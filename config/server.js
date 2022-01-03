a95e306d44a36f686f7e714ffabd6d718de8dba3
lexobegin <65969094+lexobegin@users.noreply.github.com>
2022-01-03 16:41:22 -0400

    e-commerce-server

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fb3406530a6d04b54e328fe1a72642df'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '626861d7-6aa0-4841-bd72-8d2cb7cf6850'
+};
